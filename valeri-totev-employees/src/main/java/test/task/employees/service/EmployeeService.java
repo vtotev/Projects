f3f633698b2bde44d020d9e77404d3ecd3f0be12
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2023-04-11 00:44:31 +0300

    Delete valeri-totev-employees directory

--- valeri-totev-employees/src/main/java/test/task/employees/service/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package test.task.employees.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface EmployeeService {
-    boolean readEmployees(MultipartFile fileData);
-}
